5f7f14e39453705f17cefa36eacb7698f8811a18
RikiShmu <s0527179598@gmail.com>
2023-11-28 17:02:37 +0200

    add evebts options

--- client/script.js
+++ client/script.js
@@ -1,4 +1,8 @@
-function updateQuestions() {
+// Move the general questions outside the updateQuestions function
+const typeInput = document.getElementById('type');
+const atmosphereInput = document.getElementById('atmosphere');
+
+async function updateQuestions() {
     const selectedEvent = document.getElementById('event').value;
     const additionalQuestionsDiv = document.getElementById('additionalQuestions');
 
@@ -17,17 +21,15 @@ function updateQuestions() {
         additionalQuestionsDiv.innerHTML += '<input type="text" id="degree">';
     }
     // Add more conditions for other events
-
-    // You can add more logic for additional questions based on the event type
 }
 
-function generateGreeting() {
+async function generateGreeting() {
     const selectedEvent = document.getElementById('event').value;
     const age = document.getElementById('age') ? document.getElementById('age').value : '';
     const relation = document.getElementById('relation') ? document.getElementById('relation').value : '';
     const degree = document.getElementById('degree') ? document.getElementById('degree').value : '';
-    const type = 'short';  // You can implement logic for type selection
-    const atmosphere = 'happy';  // You can implement logic for atmosphere selection
+    const type = typeInput.value;
+    const atmosphere = atmosphereInput.value;
 
     // Display selected options
     const selectedOptionsDiv = document.getElementById('selectedOptions');
@@ -35,26 +37,38 @@ function generateGreeting() {
     if (age) selectedOptionsDiv.innerHTML += `<p>Age: ${age}</p>`;
     if (relation) selectedOptionsDiv.innerHTML += `<p>Relation: ${relation}</p>`;
     if (degree) selectedOptionsDiv.innerHTML += `<p>Degree: ${degree}</p>`;
-    selectedOptionsDiv.innerHTML += `<p>Type: ${type}</p>`;
-    selectedOptionsDiv.innerHTML += `<p>Atmosphere: ${atmosphere}</p>`;
+    if (type) selectedOptionsDiv.innerHTML += `<p>Type: ${type}</p>`;
+    if (atmosphere) selectedOptionsDiv.innerHTML += `<p>Atmosphere: ${atmosphere}</p>`;
 
-    // Call the server to generate a greeting
-    fetch('/generate-greeting', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ event: selectedEvent, age, relation, degree, type, atmosphere }),
-    })
-    .then(response => response.json())
-    .then(data => {
+    try {
+        // Call the server to generate a greeting
+        const response = await fetch('/generate-greeting', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ event: selectedEvent, age, relation, degree, type, atmosphere }),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
         // Display the generated greeting
         const generatedGreetingDiv = document.getElementById('generatedGreeting');
         generatedGreetingDiv.innerHTML = `<p>${data.greeting}</p>`;
-    });
+    } catch (error) {
+        console.error('Error:', error);
+        // Handle error gracefully, e.g., display an error message to the user
+    }
 }
 
 function changeGreeting() {
     // Reset the generated greeting
     document.getElementById('generatedGreeting').innerHTML = '';
 }
+
+// Initial call to load the general questions
+updateQuestions();
--- server/server.js
+++ server/server.js
@@ -7,18 +7,51 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
+// Set up your OpenAI API key
+const openAIAPI = new OpenAIAPI('your-openai-api-key');
+
 // Handle POST requests for generating greetings
 app.post('/generate-greeting', async (req, res) => {
-    const { event, age, type, atmosphere } = req.body;
+    try {
+        const { event, age, relation, degree, type, atmosphere, numResponses = 1 } = req.body;
 
-    // Build a prompt for OpenAI GPT
-    const prompt = `Write me a${age ? ` ${age}-year-old` : ''} ${event} greeting in a ${type}, ${atmosphere} atmosphere`;
+        // Build a prompt for OpenAI GPT based on the selected event and options
+        let prompt = `Write me a`;
+        if (age) prompt += ` ${age}-year-old`;
+        prompt += ` ${event} greeting`;
+        
+        // Add additional details based on the event
+        switch (event) {
+            case 'birthday':
+                // Include age-specific details
+                break;
+            case 'wedding':
+                // Include relation-specific details
+                if (relation) prompt += ` for the ${relation}`;
+                break;
+            case 'graduation':
+                // Include degree-specific details
+                if (degree) prompt += ` for the graduate with a ${degree} degree`;
+                break;
+            // Add more cases for other events
+            default:
+                // Handle unknown events
+                break;
+        }
 
-    // Use the OpenAI API to generate a greeting
-    const generatedGreeting = await openAIAPI.generate(prompt);
+        // Include general options
+        if (type) prompt += ` of type ${type}`;
+        if (atmosphere) prompt += ` in a ${atmosphere} atmosphere`;
 
-    // Send the generated greeting back to the client
-    res.json({ greeting: generatedGreeting });
+        // Use the OpenAI API to generate greetings
+        const generatedGreetings = await openAIAPI.generate(prompt, numResponses);
+
+        // Send the generated greetings back to the client
+        res.json({ greetings: generatedGreetings });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 // Handle other routes as needed
